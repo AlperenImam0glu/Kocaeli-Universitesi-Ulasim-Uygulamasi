d25e97dd2f41809ad718a435b873e6e4ba78597c
Alperen Imamoglu <alperenimamoglu16@gmail.com>
2022-04-13 02:08:49 +0300

    admin ekranı butonlar eklendi

--- lib/admin_page.dart
+++ lib/admin_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:yazlab2_proje2_mobil/Duraklar.dart';
 import 'package:yazlab2_proje2_mobil/Duraklar.dart';
+import 'package:yazlab2_proje2_mobil/admin_harita.dart';
+import 'package:yazlab2_proje2_mobil/admin_yolcu_sayilari.dart';
 
 class AdminPage extends StatefulWidget {
   @override
@@ -10,21 +12,6 @@ class AdminPage extends StatefulWidget {
 }
 
 class _AdminPageState extends State<AdminPage> {
-  Duraklar izmit = new Duraklar(40.764759, 29.940957);
-  Duraklar basiskele = new Duraklar(40.709633, 29.923609);
-  Duraklar cayirova = new Duraklar(40.823270, 29.372250);
-  Duraklar darica = new Duraklar(40.773861, 29.401166);
-  Duraklar derince = new Duraklar(40.755790, 29.831286);
-
-  final duraklistesi = <Duraklar>[];
-  _AdminPageState() {
-    duraklistesi.add(izmit);
-    duraklistesi.add(basiskele);
-    duraklistesi.add(cayirova);
-    duraklistesi.add(darica);
-    duraklistesi.add(derince);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,54 +21,50 @@ class _AdminPageState extends State<AdminPage> {
         centerTitle: true,
         title: Text("Admin Kontrol Ekranı"),
       ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 2,
-            child: mapWidget(),
-          ),
-          Expanded(
-            flex: 1,
-            child: Text("BURADA İSLEMLER OLACAK"),
-          ),
-        ],
-      ),
-    );
-  }
-
-  FlutterMap mapWidget() {
-    return new FlutterMap(
-      options: new MapOptions(
-        minZoom: 5.0,
-        center: new LatLng(40.769320, 29.943776),
-      ),
-      layers: [
-        new TileLayerOptions(
-            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-            subdomains: ['a', 'b', 'c']),
-        new MarkerLayerOptions(
-          markers: duraklar(),
-        )
-      ],
-    );
-  }
-
-  List<Marker> duraklar() {
-    final markerlar = <Marker>[];
-    for (var item in duraklistesi) {
-      Marker marker = new Marker(
-        width: 45,
-        height: 45,
-        point: new LatLng(item.lat, item.long),
-        builder: (context) => new Container(
-          child: IconButton(
-              icon: Icon(Icons.add_location_sharp, color: Colors.purple),
-              onPressed: () {}),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 30,
+            ),
+            FlatButton(
+              child: Text(
+                'Harita Görüntüle',
+                style: TextStyle(fontSize: 20.0),
+              ),
+              color: Colors.blueAccent,
+              textColor: Colors.white,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Harita(),
+                  ),
+                );
+              },
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            FlatButton(
+              child: Text(
+                'Yolcu Listesi Görüntüle',
+                style: TextStyle(fontSize: 20.0),
+              ),
+              color: Colors.blueAccent,
+              textColor: Colors.white,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => YolcuSayilari(),
+                  ),
+                );
+              },
+            ),
+          ],
         ),
-      );
-      markerlar.add(marker);
-    }
-
-    return markerlar;
+      ),
+    );
   }
 }
--- lib/admin_yolcu_sayilari.dart
+++ lib/admin_yolcu_sayilari.dart
@@ -9,21 +9,6 @@ class YolcuSayilari extends StatefulWidget {
 }
 
 class _YolcuSayilariState extends State<YolcuSayilari> {
-  Duraklar izmit = new Duraklar(40.764759, 29.940957);
-  Duraklar basiskele = new Duraklar(40.709633, 29.923609);
-  Duraklar cayirova = new Duraklar(40.823270, 29.372250);
-  Duraklar darica = new Duraklar(40.773861, 29.401166);
-  Duraklar derince = new Duraklar(40.755790, 29.831286);
-
-  final duraklistesi = <Duraklar>[];
-  _YolcuSayilariState() {
-    duraklistesi.add(izmit);
-    duraklistesi.add(basiskele);
-    duraklistesi.add(cayirova);
-    duraklistesi.add(darica);
-    duraklistesi.add(derince);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,53 +18,11 @@ class _YolcuSayilariState extends State<YolcuSayilari> {
         centerTitle: true,
         title: Text("Admin Kontrol Ekranı"),
       ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 2,
-            child: mapWidget(),
-          ),
-          Expanded(
-            flex: 1,
-            child: Text("BURADA İSLEMLER OLACAK"),
-          ),
-        ],
-      ),
-    );
-  }
-
-  FlutterMap mapWidget() {
-    return new FlutterMap(
-      options: new MapOptions(
-        minZoom: 5.0,
-        center: new LatLng(40.769320, 29.943776),
-      ),
-      layers: [
-        new TileLayerOptions(
-            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-            subdomains: ['a', 'b', 'c']),
-        new MarkerLayerOptions(
-          markers: duraklar(),
-        )
-      ],
-    );
-  }
-
-  List<Marker> duraklar() {
-    final markerlar = <Marker>[];
-    for (var item in duraklistesi) {
-      Marker marker = new Marker(
-        width: 45,
-        height: 45,
-        point: new LatLng(item.lat, item.long),
-        builder: (context) => new Container(
-          child: IconButton(
-              icon: Icon(Icons.add_location_sharp, color: Colors.purple),
-              onPressed: () {}),
+      body: Center(
+        child: Column(
+          children: [Text("Burada yolcu sayıları olacak")],
         ),
-      );
-      markerlar.add(marker);
-    }
-    return markerlar;
+      ),
+    );
   }
 }
